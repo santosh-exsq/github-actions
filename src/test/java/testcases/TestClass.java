b90de13787bc147096ff067dd74f02f85e12e4b9
Santosh Mmac Ex2 <sgsantoshgupta1097@gmail.com>
2022-03-06 00:27:10 +0530

    headless mode execution

--- src/test/java/testcases/TestClass.java
+++ src/test/java/testcases/TestClass.java
@@ -3,6 +3,7 @@ package testcases;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 public class TestClass {
@@ -11,7 +12,9 @@ public class TestClass {
     @Test
     public void testingMethod(){
         WebDriverManager.chromedriver().setup();
-        driver= new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        driver= new ChromeDriver(options);
         driver.get("https://google.com");
         System.out.println("Browser launched with "+driver.getTitle());
         driver.quit();
